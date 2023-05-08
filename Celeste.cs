5eebf0c9cfefe55076f82123f298992b86961600
Crowd Control Effect Pack Synchronization Tool <kat@warp.world>
2023-05-08 19:36:26 +0000

    Crowd Control Effect Pack Synchronization Tool - Automated Commit

--- Celeste.cs
+++ Celeste.cs
@@ -3,54 +3,53 @@ using CrowdControl.Common;
 using JetBrains.Annotations;
 using ConnectorType = CrowdControl.Common.ConnectorType;
 
-namespace CrowdControl.Games.Packs
+namespace CrowdControl.Games.Packs;
+
+[UsedImplicitly]
+public class Celeste : SimpleTCPPack
 {
-    [UsedImplicitly]
-    public class Celeste : SimpleTCPPack
+    public override string Host => "127.0.0.1";
+
+    public override ushort Port => 58430;
+
+    //public override ISimpleTCPPack.MessageFormat MessageFormat => ISimpleTCPPack.MessageFormat.CrowdControlLegacy;
+
+    public Celeste(UserRecord player, Func<CrowdControlBlock, bool> responseHandler, Action<object> statusUpdateHandler) : base(player, responseHandler, statusUpdateHandler) { }
+
+    public override Game Game { get; } = new("Celeste", "Celeste", "PC", ConnectorType.SimpleTCPServerConnector);
+
+    public override EffectList Effects { get; } = new Effect[]
     {
-        public override string Host => "127.0.0.1";
+        new("Oshiro", "oshiro") { Duration = 30 },
+        new("Giant Oshiro", "oshiro_giant") { Duration = 30 },
+        new("Seeker", "seeker") { Duration = 30 },
+        new("Snowballs", "snowballs") { Duration = 30 },
+        new("Badeline", "chaser") { Duration = 30 },
+        new("Kill Player", "kill"),
+        new("Reset Level", "reset"),
+        new("Wind", "wind"),
+        new("Taunting Laughter", "laughter") { Duration = 15 },
+        new("Unlimited Dashes", "dashes") { Duration = 30 },
+        new("Infinite Stamina", "stamina") { Duration = 30 },
+        new("Invincibility", "invincible") { Duration = 30 },
+        new("Invisibility", "invisible") { Duration = 30 },
+        new("No Stamina", "nostamina") { Duration = 15 },
+        new("Zoom Camera", "zoom") { Duration = 30 },
+        new("Earthquake", "earthquake") { Duration = 30 },
+        new("Speed Up Time", "speed") { Duration = 30 },
+        new("Slow Down Time", "slow") { Duration = 30 },
+        new("Hiccups", "hiccups") { Duration = 30 },
+        new("Ice Physics", "icephysics") { Duration = 30 },
+        new("Invert D-Pad", "invertdpad") { Duration = 15 },
+        new("Flip Screen", "flipscreen") { Duration = 15 },
+        new("Mirror World", "mirrorworld") { Duration = 30 },
 
-        public override ushort Port => 58430;
-
-        //public override ISimpleTCPPack.MessageFormat MessageFormat => ISimpleTCPPack.MessageFormat.CrowdControlLegacy;
-
-        public Celeste(UserRecord player, Func<CrowdControlBlock, bool> responseHandler, Action<object> statusUpdateHandler) : base(player, responseHandler, statusUpdateHandler) { }
-
-        public override Game Game { get; } = new("Celeste", "Celeste", "PC", ConnectorType.SimpleTCPServerConnector);
-
-        public override EffectList Effects { get; } = new Effect[]
+        new("Player Sprite", "sprite", ItemKind.BidWar)
         {
-            new("Oshiro", "oshiro") { Duration = 30 },
-            new("Giant Oshiro", "oshiro_giant") { Duration = 30 },
-            new("Seeker", "seeker") { Duration = 30 },
-            new("Snowballs", "snowballs") { Duration = 30 },
-            new("Badeline", "chaser") { Duration = 30 },
-            new("Kill Player", "kill"),
-            new("Reset Level", "reset"),
-            new("Wind", "wind"),
-            new("Taunting Laughter", "laughter") { Duration = 15 },
-            new("Unlimited Dashes", "dashes") { Duration = 30 },
-            new("Infinite Stamina", "stamina") { Duration = 30 },
-            new("Invincibility", "invincible") { Duration = 30 },
-            new("Invisibility", "invisible") { Duration = 30 },
-            new("No Stamina", "nostamina") { Duration = 15 },
-            new("Zoom Camera", "zoom") { Duration = 30 },
-            new("Earthquake", "earthquake") { Duration = 30 },
-            new("Speed Up Time", "speed") { Duration = 30 },
-            new("Slow Down Time", "slow") { Duration = 30 },
-            new("Hiccups", "hiccups") { Duration = 30 },
-            new("Ice Physics", "icephysics") { Duration = 30 },
-            new("Invert D-Pad", "invertdpad") { Duration = 15 },
-            new("Flip Screen", "flipscreen") { Duration = 15 },
-            new("Mirror World", "mirrorworld") { Duration = 30 },
-
-            new("Player Sprite", "sprite", ItemKind.BidWar)
-            {
-                Parameters = new ParameterDef("Sprite", "sprite",
-                    new Parameter("Badeline", "badeline"),
-                    new Parameter("Madeline", "madeline")
-                )
-            }
-        };
-    }
+            Parameters = new ParameterDef("Sprite", "sprite",
+                new Parameter("Badeline", "badeline"),
+                new Parameter("Madeline", "madeline")
+            )
+        }
+    };
 }
