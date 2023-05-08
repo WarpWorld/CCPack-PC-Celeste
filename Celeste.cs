b44f98dc1832db142ae0b9d8d84f0bc60a7d8dfc
Crowd Control Effect Pack Synchronization Tool <kat@warp.world>
2023-05-08 16:39:35 +0000

    Crowd Control Effect Pack Synchronization Tool - Automated Commit

--- Celeste.cs
+++ Celeste.cs
@@ -12,46 +12,45 @@ namespace CrowdControl.Games.Packs
 
         public override ushort Port => 58430;
 
-        public override ISimpleTCPPack.MessageFormat MessageFormat => ISimpleTCPPack.MessageFormat.CrowdControlLegacy;
+        //public override ISimpleTCPPack.MessageFormat MessageFormat => ISimpleTCPPack.MessageFormat.CrowdControlLegacy;
 
         public Celeste(UserRecord player, Func<CrowdControlBlock, bool> responseHandler, Action<object> statusUpdateHandler) : base(player, responseHandler, statusUpdateHandler) { }
 
-        public override Game Game { get; } = new(5, "Celeste", "Celeste", "PC", ConnectorType.SimpleTCPConnector);
+        public override Game Game { get; } = new("Celeste", "Celeste", "PC", ConnectorType.SimpleTCPServerConnector);
 
         public override EffectList Effects { get; } = new Effect[]
         {
-            new Effect("Oshiro", "oshiro"){Duration = 30},
-            new Effect("Giant Oshiro", "oshiro_giant"){Duration = 30},
-            new Effect("Seeker", "seeker"){Duration = 30},
-            new Effect("Snowballs", "snowballs"){Duration = 30},
-            new Effect("Badeline", "chaser"){Duration = 30},
-            new Effect("Kill Player", "kill"),
-            new Effect("Reset Level", "reset"),
-            new Effect("Wind", "wind"),
-            new Effect("Taunting Laughter", "laughter"){Duration = 15},
-            new Effect("Unlimited Dashes", "dashes"){Duration = 30},
-            new Effect("Infinite Stamina", "stamina"){Duration = 30},
-            new Effect("Invincibility", "invincible"){Duration = 30},
-            new Effect("Invisibility", "invisible"){Duration = 30},
-            new Effect("No Stamina", "nostamina"){Duration = 15},
-            new Effect("Zoom Camera", "zoom"){Duration = 30},
-            new Effect("Earthquake", "earthquake"){Duration = 30},
-            new Effect("Speed Up Time", "speed"){Duration = 30},
-            new Effect("Slow Down Time", "slow"){Duration = 30},
-            new Effect("Hiccups", "hiccups"){Duration = 30},
-            new Effect("Ice Physics", "icephysics"){Duration = 30},
-            new Effect("Invert D-Pad", "invertdpad"){Duration = 15},
-            new Effect("Flip Screen", "flipscreen"){Duration = 15},
-            new Effect("Mirror World", "mirrorworld"){Duration = 30},
-            //new Effect("No Gravity", "nogravity"){Duration = 30},
+            new("Oshiro", "oshiro") { Duration = 30 },
+            new("Giant Oshiro", "oshiro_giant") { Duration = 30 },
+            new("Seeker", "seeker") { Duration = 30 },
+            new("Snowballs", "snowballs") { Duration = 30 },
+            new("Badeline", "chaser") { Duration = 30 },
+            new("Kill Player", "kill"),
+            new("Reset Level", "reset"),
+            new("Wind", "wind"),
+            new("Taunting Laughter", "laughter") { Duration = 15 },
+            new("Unlimited Dashes", "dashes") { Duration = 30 },
+            new("Infinite Stamina", "stamina") { Duration = 30 },
+            new("Invincibility", "invincible") { Duration = 30 },
+            new("Invisibility", "invisible") { Duration = 30 },
+            new("No Stamina", "nostamina") { Duration = 15 },
+            new("Zoom Camera", "zoom") { Duration = 30 },
+            new("Earthquake", "earthquake") { Duration = 30 },
+            new("Speed Up Time", "speed") { Duration = 30 },
+            new("Slow Down Time", "slow") { Duration = 30 },
+            new("Hiccups", "hiccups") { Duration = 30 },
+            new("Ice Physics", "icephysics") { Duration = 30 },
+            new("Invert D-Pad", "invertdpad") { Duration = 15 },
+            new("Flip Screen", "flipscreen") { Duration = 15 },
+            new("Mirror World", "mirrorworld") { Duration = 30 },
 
-            new Effect("Player Sprite", "sprite", ItemKind.BidWar)
+            new("Player Sprite", "sprite", ItemKind.BidWar)
             {
                 Parameters = new ParameterDef("Sprite", "sprite",
-                    new Parameter("Badeline", "sprite_badeline"),
-                    new Parameter("Madeline", "sprite_madeline")
+                    new Parameter("Badeline", "badeline"),
+                    new Parameter("Madeline", "madeline")
                 )
-            },
+            }
         };
     }
 }
