36d34e7e65cd821b1df4a8721b9b5a9f2eed900e
Crowd Control Effect Pack Synchronization Tool <kat@warp.world>
2024-04-06 17:14:06 +0000

    Crowd Control Effect Pack Synchronization Tool - Automated Commit

--- CelesteMod/Actions/EffectSpriteWar.cs
+++ CelesteMod/Actions/EffectSpriteWar.cs
@@ -1,48 +1,47 @@
 ﻿using Microsoft.Xna.Framework;
 using Monocle;
 
-namespace Celeste.Mod.CrowdControl.Actions
+namespace Celeste.Mod.CrowdControl.Actions;
+
+// ReSharper disable once UnusedMember.Global
+public class EffectSpriteWarMadeline : Effect
 {
-    // ReSharper disable once UnusedMember.Global
-    public class EffectSpriteWarMadeline : Effect
+    public override string Code { get; } = "sprite_madeline";
+
+    public override string Group { get; } = "sprite";
+
+    public override EffectType Type { get; } = EffectType.BidWar;
+
+    public override void Update(GameTime gameTime)
     {
-        public override string Code { get; } = "sprite_madeline";
+        base.Update(gameTime);
+        if (!Active || (!(Engine.Scene is Level level)) || (Player == null)) { return; }
 
-        public override string Group { get; } = "sprite";
-
-        public override EffectType Type { get; } = EffectType.BidWar;
-
-        public override void Update(GameTime gameTime)
+        if (Player.DefaultSpriteMode != PlayerSpriteMode.Madeline)
         {
-            base.Update(gameTime);
-            if (!Active || (!(Engine.Scene is Level level)) || (Player == null)) { return; }
-
-            if (Player.DefaultSpriteMode != PlayerSpriteMode.Madeline)
-            {
-                Player.DefaultSpriteMode = PlayerSpriteMode.Madeline;
-                Player.ResetSprite(PlayerSpriteMode.Madeline);
-            }
+            Player.DefaultSpriteMode = PlayerSpriteMode.Madeline;
+            Player.ResetSprite(PlayerSpriteMode.Madeline);
         }
     }
+}
 
-    public class EffectSpriteWarBadeline : Effect
+public class EffectSpriteWarBadeline : Effect
+{
+    public override string Code { get; } = "sprite_badeline";
+
+    public override string Group { get; } = "sprite";
+
+    public override EffectType Type { get; } = EffectType.BidWar;
+
+    public override void Update(GameTime gameTime)
     {
-        public override string Code { get; } = "sprite_badeline";
+        base.Update(gameTime);
+        if (!Active || (!(Engine.Scene is Level level)) || (Player == null)) { return; }
 
-        public override string Group { get; } = "sprite";
-
-        public override EffectType Type { get; } = EffectType.BidWar;
-
-        public override void Update(GameTime gameTime)
+        if (Player.DefaultSpriteMode != PlayerSpriteMode.MadelineAsBadeline)
         {
-            base.Update(gameTime);
-            if (!Active || (!(Engine.Scene is Level level)) || (Player == null)) { return; }
-
-            if (Player.DefaultSpriteMode != PlayerSpriteMode.MadelineAsBadeline)
-            {
-                Player.DefaultSpriteMode = PlayerSpriteMode.MadelineAsBadeline;
-                Player.ResetSprite(PlayerSpriteMode.MadelineAsBadeline);
-            }
+            Player.DefaultSpriteMode = PlayerSpriteMode.MadelineAsBadeline;
+            Player.ResetSprite(PlayerSpriteMode.MadelineAsBadeline);
         }
     }
 }
--- CelesteMod/CrowdControlSettings.cs
+++ CelesteMod/CrowdControlSettings.cs
@@ -1,26 +1,25 @@
 ﻿using YamlDotNet.Serialization;
 
-namespace Celeste.Mod.CrowdControl
+namespace Celeste.Mod.CrowdControl;
+
+public class CrowdControlSettings : EverestModuleSettings
 {
-    public class CrowdControlSettings : EverestModuleSettings
-    {
-        [SettingIgnore]
-        [YamlMember(Alias = "Enabled")] 
-        protected bool _Enabled { get; set; } = false;
-        public bool Enabled {
-            get => _Enabled;
-            set {
-                if (_Enabled == value)
-                    return;
+    [SettingIgnore]
+    [YamlMember(Alias = "Enabled")] 
+    protected bool _Enabled { get; set; } = false;
+    public bool Enabled {
+        get => _Enabled;
+        set {
+            if (_Enabled == value)
+                return;
 
-                if (value)
-                    CrowdControlHelper.Add();
-                else
-                    CrowdControlHelper.Remove();
+            if (value)
+                CrowdControlHelper.Add();
+            else
+                CrowdControlHelper.Remove();
 
-                _Enabled = value;
-            }
+            _Enabled = value;
         }
-
     }
+
 }
